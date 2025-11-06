cbd4373a60ed159637b826a33ffa7d00cf80f544
TinhNguyenTB <tinh271103@gmail.com>
2025-11-06 23:09:47 +0700

    genreate token and config axios interceptors handle error global

---
+++ web-base/src/utils/authorizedAxios.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { API_ROOT } from "~/utils/constants";
+
+const axiosInstance = axios.create({
+  baseURL: API_ROOT,
+  timeout: 10 * 60 * 1000, // 10 min
+  //   withCredentials: true,
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+
+    // Xử lý tập trung phần hiển thị thông báo lỗi trả về từ mọi API, ngoại trừ 410 - GONE
+    if (error.response?.status !== 410) {
+      toast.error(error.response?.data?.message || error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
